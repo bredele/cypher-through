a7b5027837a00c741e253af39326bd33eaad8c0f
bredele <olivier.wietrich@gmail.com>
2018-03-08 07:38:42 -0700

    Transform records into objects

---
+++ index.js
@@ -0,0 +1,37 @@
+
+/**
+ * Filter Neo4j query result.
+ *
+ * @param {Object} result
+ * @return {Object|Array}
+ * @api public
+ */
+
+module.exports = result => {
+  return result.records.map(objectify)
+}
+
+
+/**
+ * Transform record into an object.
+ *
+ * A record ha a method to Object but returns identity, labels as well as
+ * properties for nodes rather than properties only.
+ *
+ * @param {Object} record
+ * @return {Object}
+ * @api private
+ */
+
+function objectify (record) {
+  const result = {}
+  record._fields.map((field, idx) => {
+    const key = record.keys[idx]
+    if (typeof field === 'object' && field.toString() !== '[object Object]') {
+      result[key] = field.properties
+    } else {
+      result[key] = field
+    }
+  })
+  return result
+}
